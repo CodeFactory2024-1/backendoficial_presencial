d5eff2f071034e07b762bc750bd2c0e5eb171821
sneyderbg <sneyder.buitrago@udea.edu.co>
2024-05-17 12:35:37 -0500

    fix: change CORS allowed origin to set it from the properties

--- src/main/java/co/udea/airline/api/utils/filter/CORSFilter.java
+++ src/main/java/co/udea/airline/api/utils/filter/CORSFilter.java
@@ -1,6 +1,8 @@
 package co.udea.airline.api.utils.filter;
 
 import jakarta.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.*;
@@ -14,14 +16,18 @@ import java.io.IOException;
 @Component
 public class CORSFilter implements Filter {
 
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-		response.setHeader("Access-Control-Max-Age", "1800");
-		chain.doFilter(req, res);
-	}
-	
+    @Value("${airline-api.cors.allow-origins}")
+    private String allowedOrigins;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins);
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+        response.setHeader("Access-Control-Max-Age", "1800");
+        chain.doFilter(req, res);
+    }
+
 }
