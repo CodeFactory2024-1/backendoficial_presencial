52369a072458d921d997662c7685bd72f1dcb469
sneyderbg <sneyder.buitrago@udea.edu.co>
2024-05-21 17:51:36 -0500

    fix: CORS configuration

--- src/main/java/co/udea/airline/api/utils/config/SecurityConfig.java
+++ src/main/java/co/udea/airline/api/utils/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .anonymous(anon -> anon.disable())
                 .authorizeHttpRequests(
                         req -> req.requestMatchers("/login/**", "/register/**").permitAll()
@@ -43,5 +42,5 @@ public class SecurityConfig {
                 .build();
 
     }
-    
+
 }
---
+++ src/main/java/co/udea/airline/api/utils/filter/CorsFilterConfig.java
@@ -0,0 +1,32 @@
+package co.udea.airline.api.utils.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Filter for Cross-Origin Resource Sharing.
+ *
+ */
+@Configuration
+public class CorsFilterConfig {
+
+    @Value("${airline-api.cors.allow-origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin(allowedOrigins);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
