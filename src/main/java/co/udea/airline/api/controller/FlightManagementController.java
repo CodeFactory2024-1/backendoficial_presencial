b03a290635f7d2a2576c3390d1924a2fa5b45f88
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-05-26 21:18:38 -0500

    add security to endpoints and configure jwt properties

--- src/main/java/co/udea/airline/api/controller/FlightManagementController.java
+++ src/main/java/co/udea/airline/api/controller/FlightManagementController.java
@@ -14,11 +14,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
@@ -26,6 +28,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@PreAuthorize("hasRole('ADMIN')")
+@SecurityRequirement(name = "JWT")
 @RequestMapping("/v1/flights")
 @Tag(name = "Flight Management", description = "Flight management operations")
 public class FlightManagementController {
